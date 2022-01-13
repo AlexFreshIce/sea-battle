e48376497d1b6defb37c6ea50eb8fb078ea68b87
AlexFreshIce <alexfreshice@yandex.ru>
2022-01-13 15:15:52 +0300

    add battleground

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let canvas =  document.querySelector("#canvas1");
+let canvas =  document.getElementById("canvas1");
 let ctx = canvas.getContext('2d');
 canvas.width = 1600;  //2800
 canvas.height = 800;  //1400
@@ -15,13 +15,22 @@ canvas.height = 800;  //1400
 //   mouse.x = canvas.width/2;
 //   mouse.y = canvas.height/2;
 // });
+const canvasPosition = canvas.getBoundingClientRect()
+console.log(canvasPosition)
 
 
 // document.addEventListener("click", (event)=>{
-//   console.log(`x = ${event.clientX-canvasPosition.left} 
-//                y = ${event.clientY-canvasPosition.top}` )})
+//   console.log(`x = ${(event.clientX-canvasPosition.x)*(canvas.width/canvasPosition.width)}`);
+//   console.log(`y = ${(event.clientY-canvasPosition.y)*(canvas.height/canvasPosition.height)}`);
+//                console.log(mouse.x);
+//                console.log(mouse.y);
+//                console.log(Player1);
+//                console.log(canvas.width/canvasPosition.width);
+//                console.log(canvas.height/canvasPosition.height);
 
-const canvasPosition = canvas.getBoundingClientRect()
+//               })
+
+
 let battlCell = 10;
 let battlCellMin = 10;
 let battlCellMax = 29;
@@ -39,7 +48,7 @@ const ships = {
 }
 
 const battlegroundPosition1 = {
-  x:canvas.width/2 - 128 - battlSize,
+  x:canvas.width/2 - battlSize - battlCellSize*4,
   y:battlCellSize*2,
 }
 const battlegroundPosition2 = {
@@ -115,8 +124,8 @@ canvas.addEventListener("mouseup", upHandler);
 
 
 function moveHandler(event){
-  mouse.x = event.clientX-canvasPosition.left;
-  mouse.y = event.clientY-canvasPosition.top;
+  mouse.x = Math.round((event.clientX-canvasPosition.left)*(canvas.width/canvasPosition.width)*100)/100;
+  mouse.y = Math.round((event.clientY-canvasPosition.y)*(canvas.height/canvasPosition.height)*100)/100;
 }
 
 function downHandler(event){
@@ -144,6 +153,7 @@ function mousePreviouslyClick() {
 }
 
 
+
 // Drag'n'Drop
 function takeObj (Player){ 
   //Take Obj on ship dock
@@ -154,18 +164,20 @@ function takeObj (Player){
       if ( (shipDock[shipDockKey[i]].position.sX <= mouse.x) 
             && (shipDock[shipDockKey[i]].position.sX + battlCellSize*(i+1) >= mouse.x)
             && (shipDock[shipDockKey[i]].position.sY <= mouse.y)
-            && (shipDock[shipDockKey[i]].position.sY + battlCellSize*(i+1) >= mouse.y)){
+            && (shipDock[shipDockKey[i]].position.sY + battlCellSize*(i+1) >= mouse.y)
+            && (shipDock[shipDockKey[i]].numberOf)
+            ){
                 mouse.takeObject=shipDock[shipDockKey[i]];
                 mouse.takeObjectHorizontal = true;
         }
         
       }
-      console.log(mouse.takeObject)  
-      console.log(mouse.takeObjectHorizontal)  
+      // console.log(mouse.takeObject)  
+      // console.log(mouse.takeObjectHorizontal)  
     }
     if (mouse.right && !mouse.pRight){
       mouse.takeObjectHorizontal = !mouse.takeObjectHorizontal
-      console.log(mouse.takeObjectHorizontal)  
+      // console.log(mouse.takeObjectHorizontal)  
     }
     if (!mouse.left && !mouse.pLeft){
     mouse.takeObject={}
@@ -178,13 +190,13 @@ function takeObj (Player){
     ctx.strokeStyle = "rgba(40, 46, 250, 1)";
     ctx.lineWidth = 4; 
     if (mouse.takeObjectHorizontal){
-      ctx.rect(mouse.x-battlCellSize*mouse.takeObject.size/2, 
+      ctx.rect(mouse.x-battlCellSize*mouse.takeObject.size + battlCellSize/2, 
       mouse.y-battlCellSize/2, 
       battlCellSize*mouse.takeObject.size, 
       battlCellSize);}
     else {
       ctx.rect(mouse.x-battlCellSize/2, 
-      mouse.y-battlCellSize*mouse.takeObject.size/2, 
+      mouse.y-battlCellSize*mouse.takeObject.size + battlCellSize/2, 
       battlCellSize, 
       battlCellSize*mouse.takeObject.size);
     }  
@@ -193,61 +205,126 @@ function takeObj (Player){
   }
 }
 
+function dropObj (Player){
+  Player.battleground[0][0].ship = true;
+  Player.battleground[0][1].block = true;
+  if (!mouse.left && mouse.pLeft){
+ for (let y = 0; y<battlCell; y++){
+  for (let x = 0; x<battlCell; x++){
+    if ((Player.battleground[y][x].cX <= mouse.x)
+    &&(Player.battleground[y][x].cY <= mouse.y)
+    &&(Player.battleground[y][x].cX + battlCellSize >= mouse.x)
+    &&(Player.battleground[y][x].cY + battlCellSize >= mouse.y)
+    &&(!Player.battleground[y][x].ship)
+    &&(!Player.battleground[y][x].block)   
+    ){if (mouse.takeObjectHorizontal
+          && mouse.takeObject.size == 1
+          &&(Player.battleground[y][x].cX - mouse.takeObject.size*battlCellSize + battlCellSize>= Player.battlegroundPosition.x)
+          && !Player.battleground[y][x].block) {
+        Player.battleground[y][x].ship = true;
+        Player.shipDock.ships1.numberOf-=1;
+      }
+      if (mouse.takeObjectHorizontal
+        && mouse.takeObject.size == 2 
+        &&(Player.battleground[y][x].cX - mouse.takeObject.size*battlCellSize + battlCellSize>= Player.battlegroundPosition.x)
+        && !Player.battleground[y][x].block
+        && !Player.battleground[y][x-1].block) {
+        Player.battleground[y][x].ship = true;
+        Player.battleground[y][x-1].ship = true;
+        Player.shipDock.ships2.numberOf-=1
+      }
+      if (mouse.takeObjectHorizontal
+        && mouse.takeObject.size == 3 
+        &&(Player.battleground[y][x].cX - mouse.takeObject.size*battlCellSize + battlCellSize>= Player.battlegroundPosition.x)
+        && !Player.battleground[y][x].block
+        && !Player.battleground[y][x-1].block
+        && !Player.battleground[y][x-2].block) {
+        Player.battleground[y][x].ship = true;
+        Player.battleground[y][x-1].ship = true;
+        Player.battleground[y][x-2].ship = true;
+        Player.shipDock.ships3.numberOf-=1
+      }
+      if (mouse.takeObjectHorizontal
+        && mouse.takeObject.size == 4 
+        &&(Player.battleground[y][x].cX - mouse.takeObject.size*battlCellSize + battlCellSize>= Player.battlegroundPosition.x)
+        && !Player.battleground[y][x].block
+        && !Player.battleground[y][x-1].block
+        && !Player.battleground[y][x-2].block
+        && !Player.battleground[y][x-3].block) {
+        Player.battleground[y][x].ship = true;
+        Player.battleground[y][x-1].ship = true;
+        Player.battleground[y][x-2].ship = true;
+        Player.battleground[y][x-3].ship = true;
+        Player.shipDock.ships4.numberOf-=1
+      }
+      if (!mouse.takeObjectHorizontal
+        && mouse.takeObject.size == 1 
+        &&(Player.battleground[y][x].cY - mouse.takeObject.size*battlCellSize + battlCellSize>= Player.battlegroundPosition.y)
+        && !Player.battleground[y][x].block) {
+        Player.battleground[y][x].ship = true;
+        Player.shipDock.ships1.numberOf-=1
+      }
+      if (!mouse.takeObjectHorizontal
+        && mouse.takeObject.size == 2 
+        &&(Player.battleground[y][x].cY - mouse.takeObject.size*battlCellSize + battlCellSize>= Player.battlegroundPosition.y)
+        && !Player.battleground[y][x].block
+        && !Player.battleground[y-1][x].block) {
+        Player.battleground[y][x].ship = true;
+        Player.battleground[y-1][x].ship = true;
+        Player.shipDock.ships2.numberOf-=1
+      }
+      if (!mouse.takeObjectHorizontal
+        && mouse.takeObject.size == 3 
+        &&(Player.battleground[y][x].cY - mouse.takeObject.size*battlCellSize + battlCellSize>= Player.battlegroundPosition.y)
+        && !Player.battleground[y][x].block
+        && !Player.battleground[y-1][x].block
+        && !Player.battleground[y-2][x].block) {
+        Player.battleground[y][x].ship = true;
+        Player.battleground[y-1][x].ship = true;
+        Player.battleground[y-2][x].ship = true;
+        Player.shipDock.ships3.numberOf-=1
+      }
+        if (!mouse.takeObjectHorizontal
+        && mouse.takeObject.size == 4 
+        &&(Player.battleground[y][x].cY - mouse.takeObject.size*battlCellSize + battlCellSize>= Player.battlegroundPosition.y)
+        && !Player.battleground[y][x].block
+        && !Player.battleground[y-1][x].block
+        && !Player.battleground[y-2][x].block
+        && !Player.battleground[y-3][x].block) {
+        Player.battleground[y][x].ship = true;
+        Player.battleground[y-1][x].ship = true;
+        Player.battleground[y-2][x].ship = true;
+        Player.battleground[y-3][x].ship = true;
+        Player.shipDock.ships4.numberOf-=1
+      }  
+    }
+  }
+}
+} 
+    
+    // if (Player.battleground[y][x].strike){}
+   
+}
+
+function blockCell (Player){
+ for (let y = 0; y<battlCell; y++){
+  for (let x = 0; x<battlCell; x++){
+ if ( Player.battleground[y][x].block 
+      || Player.battleground[y][x].ship ){continue}
+ if (  Player.battleground?.[y]?.[x-1]?.ship
+      || (Player.battleground?.[y-1]?.[x-1]?.ship)
+      || (Player.battleground?.[y-1]?.[x]?.ship)
+      || (Player.battleground?.[y-1]?.[x+1]?.ship)
+      || (Player.battleground?.[y]?.[x+1]?.ship)
+      || (Player.battleground?.[y+1]?.[x+1]?.ship)
+      || (Player.battleground?.[y+1]?.[x]?.ship)
+      || (Player.battleground?.[y+1]?.[x-1]?.ship)
+      ) {Player.battleground[y][x].block = true;}
+  }}}
 
 
- 
 
-
-
-// function takeObjHandler(event){
-//   let mX = event.clientX-canvasPosition.left;
-//   let mY = event.clientY-canvasPosition.top;
-//   let {shipDock} = Player1;
-//   let shipDockKey = Object.keys(shipDock)
-//   for (i = 0; i<shipDockKey.length; i++){
-//       if ( (shipDock[shipDockKey[i]].position.sX <= mX) 
-//             && (shipDock[shipDockKey[i]].position.sX + battlCellSize*(i+1) >= mX)
-//             && (shipDock[shipDockKey[i]].position.sY <= mY)
-//             && (shipDock[shipDockKey[i]].position.sY + battlCellSize*(i+1) >= mY)){
-//                 mouse.takeObject=shipDock[shipDockKey[i]]
-//         }
-//       } 
-//       console.log(mouse.takeObject)  
-//  }
-
-
-
-
-
-
-
-// function takeObjHandler(event){
-//   let mX = event.clientX-canvasPosition.left;
-//   let mY = event.clientY-canvasPosition.top;
-//   let {shipDock} = Player1;
-//   let shipDockKey = Object.keys(shipDock)
-//   for (i = 0; i<shipDockKey.length; i++){
-//       if ( (shipDock[shipDockKey[i]].position.sX <= mX) 
-//             && (shipDock[shipDockKey[i]].position.sX + battlCellSize*(i+1) >= mX)
-//             && (shipDock[shipDockKey[i]].position.sY <= mY)
-//             && (shipDock[shipDockKey[i]].position.sY + battlCellSize*(i+1) >= mY)){
-//                 mouse.takeObject=shipDock[shipDockKey[i]]
-//         }
-//       }
-//   console.log(mouse.takeObject)  
-//  }
-
-
-//  function moveObjHandler(event){
-//   if (Object.keys(mouse.takeObject).length != 0){
-//     mouse.x = event.clientX-canvasPosition.left;
-//     mouse.y = event.clientY-canvasPosition.top;
-//     console.log(mouse)
-//   }
-//  }
-
-
-
+  // Player.battleground[y][x+1]?.cX <= Player.battlegroundPosition.x+battlSize &&
 
 
 
@@ -280,16 +357,22 @@ function drawBattleground(Player){
       let cY = y * (battlCellSize) + Player.battlegroundPosition.y;
       Player.battleground[y][x].cX = cX;
       Player.battleground[y][x].cY = cY;
-      // ctx.beginPath();
-      // ctx.rect(cX , cY , battlCellSize , battlCellSize);
-      // ctx.strokeStyle = "rgba(40, 46, 250, 0.05)";
-      // ctx.lineWidth = 1;    
-      // ctx.fillStyle = "rgba(250, 250, 250, 1)" 
-      // ctx.fill()
-      // ctx.stroke();
-      if (Player.battleground[y][x].ship){}
+      
+      if (Player.battleground[y][x].ship){
+      ctx.beginPath();
+      ctx.strokeStyle = "rgba(6, 25, 247, 1)";
+      ctx.lineWidth = 4;
+      ctx.rect(cX , cY , battlCellSize , battlCellSize);
+      ctx.stroke();
+      }
       if (Player.battleground[y][x].strike){}
-      if (Player.battleground[y][x].block){}
+      if (Player.battleground[y][x].block){
+        ctx.beginPath();
+        ctx.fillStyle = "rgba(6, 25, 247, 1)";
+        ctx.arc(cX+battlCellSize/2,cY+battlCellSize/2, 3, 0, Math.PI*2 );
+        ctx.fill();
+        
+      }
     }
   }
  
@@ -431,6 +514,8 @@ function tick () {
 drawPreparationScreen({
   update(){
     takeObj (Player1)
+    dropObj (Player1)
+    blockCell (Player1)
 
     mousePreviouslyClick()
   },
@@ -450,8 +535,8 @@ drawPreparationScreen({
 })
 
 
-// console.log(Player1)
- console.log(Player2)
+console.log(Player1)
+//  console.log(Player2)
 // setInterval(draw, 10);
 
 
